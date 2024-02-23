74f2a3263c4aab58d843464ad678e9c8d966cae6
Abbinash Ranjitkar <abbinashranjitkar@gmail.com>
2024-02-23 13:35:49 -0700

    TDD for countCities()

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -66,5 +66,21 @@ public class CustomListTest {
         });
     }
 
+    @Test
+    public void countCitiesTest() {
+        list = MockCityList();
+        City a = new City("test", "test1");
+        assertEquals(1, list.countCities());
+
+        City b = new City("test2", "test3");
+        assertEquals(2, list.countCities());
+
+        list.deleteCity(b);
+        assertEquals(1, list.countCities());
+
+        list.deleteCity(a);
+        assertEquals(0, list.countCities());
+    }
+
 
 }
